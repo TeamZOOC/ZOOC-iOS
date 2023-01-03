1d00dc944ee06f1c5b249a938c5c6289b690785a
HELLOHIDI <fbgmlwo123@naver.com>
2023-01-03 23:24:43 +0900

    [Feat] 앱 정보 뷰 UI 컴포넌트 선언 #9

---
+++ ZOOC/ZOOC/Presentation/My/View/AppInformationView.swift
@@ -0,0 +1,27 @@
+//
+//  AppInformationView.swift
+//  ZOOC
+//
+//  Created by 류희재 on 2023/01/03.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class AppInformationView: UIView {
+    
+    //MARK: - UI Components
+    
+    private var backButton = UIButton()
+    
+    private var appInformationLabel = UILabel()
+    
+    private var currentVersionTitleLabel = UILabel()
+    
+    private var currentVersionLabel = UILabel()
+    
+    private var settingTableView = UITableView()
+    
+}
