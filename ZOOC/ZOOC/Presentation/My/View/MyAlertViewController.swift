ed5f068b3a80855b02ed5638ce9e58bbfbee6342
HELLOHIDI <fbgmlwo123@naver.com>
2023-01-06 00:29:16 +0900

    [Feat] 알림창 구현 완료 #9

--- ZOOC/ZOOC/Presentation/My/View/MyAlertViewController.swift
+++ ZOOC/ZOOC/Presentation/My/View/MyAlertViewController.swift
@@ -15,23 +15,39 @@ final class MyAlertViewController: UIViewController {
     //MARK: - Properties
     
     //MARK: - UI Components
+    private var alertView = UIView().then {
+        $0.backgroundColor = .zoocBackgroundGreen
+        $0.layer.cornerRadius = 14
+        $0.clipsToBounds = true
+    }
     
     private var alertTitleLabel = UILabel().then {
-        $0.backgroundColor = .red
+        $0.font = .zoocSubhead2
         $0.text = "회원 탈퇴 하시겠습니까?"
+        $0.textColor = .zoocDarkGray1
     }
     
     private var alertSubTitleLabel = UILabel().then {
-        $0.backgroundColor = .yellow
-        $0.text = "회원 탈퇴 시 자동으로 탈퇴되고 작성한 글과 댓글이 모두 삭제됩니다"
+        $0.font = .zoocBody1
+        $0.text = "회원 탈퇴 시 자동으로 가족에서 탈퇴되고 \n 작성한 글과 댓글이 모두 삭제됩니다"
+        $0.textColor = .zoocGray1
+        $0.textAlignment = .center
+        $0.numberOfLines = \2
     }
     
     private var keepEditButton = UIButton().then {
-        $0.backgroundColor = .green
+        $0.backgroundColor = .zoocMainGreen
+        $0.setTitle("계속 할래요", for: .normal)
+        $0.setTitleColor(.zoocWhite1, for: .normal)
+        $0.titleLabel?.textAlignment = .center
+        
     }
     
     private var popToMyViewButton = UIButton().then {
-        $0.backgroundColor = .blue
+        $0.backgroundColor = .zoocWhite2 //zoocWhite3
+        $0.setTitle("탈퇴", for: .normal)
+        $0.setTitleColor(.zoocDarkGray2, for: .normal)
+        $0.titleLabel?.textAlignment = .center
     }
     
     //MARK: - Life Cycle
@@ -46,11 +62,19 @@ final class MyAlertViewController: UIViewController {
     //MARK: - Custom Method
     
     private func setUI(){
-        view.backgroundColor = .white
+        view.backgroundColor = .zoocGray1
     }
     
     private func setLayout(){
-        view.addSubviews(alertTitleLabel, alertSubTitleLabel, keepEditButton, popToMyViewButton)
+        view.addSubview(alertView)
+        alertView.addSubviews(alertTitleLabel, alertSubTitleLabel, keepEditButton, popToMyViewButton)
+        
+        alertView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(299)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(299)
+            $0.height.equalTo(180)
+        }
         
         alertTitleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(32)
@@ -63,7 +87,7 @@ final class MyAlertViewController: UIViewController {
         }
         
         keepEditButton.snp.makeConstraints {
-            $0.top.equalTo(self.alertSubTitleLabel.snp.bottom).offset(23)
+            $0.top.equalToSuperview().offset(126)
             $0.leading.equalToSuperview()
             $0.width.equalTo(179)
             $0.height.equalTo(54)
