ae21048a922d9d3994b05221075d1be4046cd065
Melt <seokwoo2000@naver.com>
2023-02-28 16:41:03 +0900

    [Feat] TextField 터치 영역 확대 #91

--- ZOOC/ZOOC/Presentation/My/View/MyEditProfileView.swift
+++ ZOOC/ZOOC/Presentation/My/View/MyEditProfileView.swift
@@ -19,7 +19,7 @@ final class MyEditProfileView: UIView {
     
     public var profileImageButton = UIButton()
     private var cameraIconImageView = UIImageView()
-    public var nameTextField = UITextField()
+    public var nameTextField = BaseTextField()
     public var underLineView = UIView()
     public var numberOfNameCharactersLabel = UILabel()
     public var completeButton = UIButton()
